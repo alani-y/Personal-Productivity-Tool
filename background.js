05a1731ff79bfa4765e12cc7d13c3f4ca77b75a9
Alani <148376550+alani-y@users.noreply.github.com>
2024-10-07 00:52:44 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) =>{
 
-    // checks if the user is on a google calendar tab for today
-    if(tab.url && tab.url.includes("calendar.google.com/calendar")){
+    // checks if the tab is on a YouTube video
+    if(tab.url && tab.url.includes("youtube.com/watch")){
         
-        // gets the unique url for today's date in google calendar
-        const dayParameter = tab.url.split("r/")[1]; 
-        const urlParameters = new URLSearchParams(dayParameter);
+        // gets the unique video ID
+        const youtubeVideoID = tab.url.split("?")[1]; 
+        const url = new URLSearchParams(youtubeVideoID);
 
-
+        // communicates with the content script that a new video has loaded
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParameters.get("v")
+        })
     }
 })
